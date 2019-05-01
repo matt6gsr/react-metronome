0f533c26a3c995a781732f2aec1334464f318067
matt6gsr <matt6gsr@gmail.com>
2019-05-01 14:03:41 +0100

    created Metronome js and css files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Metronome from './Metronome';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Metronome />, document.getElementById('root'));
