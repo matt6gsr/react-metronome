80430ae7b8d20015e5b9684e9657bd765117b4cc
matt6gsr <matt6gsr@gmail.com>
2019-05-02 11:52:21 +0100

    metronome component rendered and basic styling added

--- src/Metronome.js
+++ src/Metronome.js
@@ -2,8 +2,29 @@ import React, { Component } from 'react';
 import './Metronome.css';
 
 class Metronome extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playing: false,
+      count: 0,
+      bpm: 100,
+      beatsPerMeasure: 4
+    };
+  }
+
   render() {
-    return <div className="metronome">hi</div>;
+    const { playing, bpm } = this.state;
+
+    return (
+      <div className="metronome">
+        <div className="bpm-slider">
+          <div>{bpm} BPM</div>
+          <input type="range" min="60" max="240" value={bpm} />
+        </div>
+        <button>{playing ? 'Stop' : 'Start'}</button>
+      </div>
+    );
   }
 }
 
